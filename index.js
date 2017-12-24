0bd0d180703bcbe385498bd0be25125c24950d19
alyonew <33595081+alyonew@users.noreply.github.com>
2017-12-24 21:33:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ var db = require('./config/db');
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-MongoClient.connect(db.url, function (error) {
-    if (error) console.error(error);
-    
-else console.log('mongo connected');
-
-});
-
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
+app.get('/', function(req, res) { 
 res.render('pad');
+});
+
+MongoClient.connect(db.url, function (error) {   
+if (error) console.error(error); 
+else console.log('mongo connected');
 });
 
 var port = process.env.PORT || 8000;
 app.listen(port);
+
+
