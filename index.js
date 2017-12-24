4c355cdbca12d1e764e164ea29c462aa51e7296a
alyonew <33595081+alyonew@users.noreply.github.com>
2017-12-24 18:57:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
-var bodyParser = require('body-parser'),
+var bodyParser = require('body-parser');
 var db = require('./config/db');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
