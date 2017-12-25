25b26c8ba68bccdd03c45de6e0ab89d805796dc2
alyonew <33595081+alyonew@users.noreply.github.com>
2017-12-25 10:20:39 +0300

    Add files via upload

---
+++ app/routes/note_routes.js
@@ -0,0 +1,96 @@
+﻿var objectId = require("mongodb").ObjectID;
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
+var mongoClient = require("mongodb").MongoClient;
+var url = "mongodb://127.0.0.1:27017/local";
+
+module.exports = function(app, db) {
+
+	app.get("/api/files", function(req, res) {
+	  mongoClient.connect(url, function(err, db) {
+	    db.collection("files").find({}).toArray(function(err, files) {
+	      if (err) {
+	        console.error(err);
+	        return res.status(400).send();
+	      }
+	      res.send(files)
+	      db.close();
+	    });
+	  });
+	});
+
+	app.get("/api/files/:id", function(req, res) {
+
+	  var id = new objectId(req.params.id);
+	  mongoClient.connect(url, function(err, db) {
+	    db.collection("files").findOne({
+	      _id: id
+	    }, function(err, file) {
+
+	      if (err) {
+	        console.error(err);
+	        return res.status(400).send();
+	      }
+	      res.send(file);
+	      db.close();
+	    });
+	  });
+	});
+
+	app.post("/api/files", jsonParser, function(req, res) {
+	  if (!req.body) return res.sendStatus(400);
+
+	  var fileName = req.body.body.fileName;
+	  var fileData = req.body.body.fileData;
+	  var file = {
+	    fileName: fileName,
+	    fileData: fileData
+	  };
+
+	  mongoClient.connect(url, function(err, db) {
+	    db.collection("files").insertOne(file, function(err, result) {
+
+	      if (err) {
+	        console.error(err);
+	        return res.status(400).send();
+	      }
+
+	      res.send(file);
+	      db.close();
+	    });
+	  });
+	});
+
+	app.put("/api/files", jsonParser, function(req, res) {
+
+	  if (!req.body) return res.sendStatus(400);
+
+	  var id = new objectId(req.body.body.id);
+	  var fileName = req.body.body.fileName;
+	  var fileData = req.body.body.fileData;
+
+	  mongoClient.connect(url, function(err, db) {
+	    db.collection("files").findOneAndUpdate({
+	      _id: id
+	    }, {
+	      $set: {
+	        fileData: fileData,
+	        fileName: fileName
+	      }
+	    }, {
+	      returnOriginal: false
+	    }, function(err, result) {
+
+	      if (err) {
+	        console.error(err);
+	        return res.status(400).send();
+	      }
+
+	      var file = result.value;
+	      console.log(file);
+	      res.send(file);
+	      db.close();
+	    });
+	  });
+	});
+};
---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoClient = require("mongodb").MongoClient;
+var objectId = require("mongodb").ObjectID;
+var app = express();
+var jsonParser = bodyParser.json();
+var url = "mongodb://127.0.0.1:27017/local";
+var port = process.env.PORT || 8000;
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+mongoClient.connect(url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('Using port ' + port);
+  });               
+})
