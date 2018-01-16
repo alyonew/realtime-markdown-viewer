b49b24a59f3ec70ad47f72473f3be135be4af6c3
alyonew <33595081+alyonew@users.noreply.github.com>
2018-01-16 15:44:53 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoClient = require("mongodb").MongoClient;
-var objectId = require("mongodb").ObjectID;
-var app = express();
-var jsonParser = bodyParser.json();
-var url = "mongodb://127.0.0.1:27017/local";
-var port = process.env.PORT || 8000;
-
+const express        = require('express');
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
+const app            = express();
+const jsonParser = bodyParser.json();
+const url = "mongodb://127.0.0.1:27017/local";
+const port = 8000;
 
 app.use(express.static(__dirname + '/public'));
-
-
-
-mongoClient.connect(url, (err, database) => {
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(url, (err, database) => {
   if (err) return console.log(err)
-  require('./app/routes')(app, database);
-  app.listen(port, () => {
-    console.log('Using port ' + port);
-  });               
+require('./app/routes')(app, database);
+app.listen(port, () => {
+  console.log('Using port ' + port);
+});
 })
